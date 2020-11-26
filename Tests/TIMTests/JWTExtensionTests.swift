23a64a4f80b6a1f0d075645064c4d7a6894ea996
Thomas Kalhøj Clemensen <tkc@trifork.com>
2020-11-26 13:18:03 +0100

    Fixed potential crash for invalid JWT's.

--- Tests/TIMTests/JWTExtensionTests.swift
+++ Tests/TIMTests/JWTExtensionTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class JWTExtensionsTests: XCTestCase {
 
     let simpleAccessToken: JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxMzM3fQ.RfalLtiTT6j_rBKX5pGm_jGmwwL-hIh-Qut7eLHiwtg"
+    let invalidJwt: JWT = "INVALID"
 
     func testUserId() {
         XCTAssertEqual(simpleAccessToken.userId, "user")
@@ -13,8 +14,13 @@ final class JWTExtensionsTests: XCTestCase {
         XCTAssertEqual(simpleAccessToken.expireTimestamp, 1337)
     }
 
+    func testInvalidToken() {
+        XCTAssertNil(invalidJwt.userId)
+    }
+
     static var allTests = [
         ("testUserId", testUserId),
         ("testExpireTime", testExpireTime),
+        ("testInvalidToken", testInvalidToken),
     ]
 }
