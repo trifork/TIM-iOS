76a12007f4abc6bc78df6b26f7073cd7ca06f48b
Thomas Kalhøj Clemensen <tkc@trifork.com>
2021-03-23 08:32:11 +0100

    Finished testing of TIMDataStorageInternal

--- Tests/TIMTests/TIMStorageInternalTests.swift
+++ Tests/TIMTests/TIMStorageInternalTests.swift
@@ -20,7 +20,7 @@ final class TIMStorageInternalTests: XCTestCase {
         for encryptionMethod in TIMESEncryptionMethod.allCases {
             let storage = dataStorage(for: encryptionMethod)
             XCTAssertFalse(storage.hasRefreshToken(userId: testRefreshToken.userId!))
-            _ = storeRefreshTokenWithNewPassword(dataStorage: storage, password: "1234")
+            _ = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
             XCTAssertTrue(storage.hasRefreshToken(userId: testRefreshToken.userId!))
         }
     }
@@ -53,7 +53,7 @@ final class TIMStorageInternalTests: XCTestCase {
             XCTAssertFalse(storage.availableUserIds.contains(updatedRefreshTokenJwt.userId))
 
             // Store the refresh token with a new password
-            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, password: "1234")
+            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
             URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
                 data: try! JSONEncoder().encode(keyModel),
                 response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
@@ -80,7 +80,7 @@ final class TIMStorageInternalTests: XCTestCase {
             let storage = dataStorage(for: encryptionMethod)
 
             // Store refresh token with new password
-            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, password: "1234")
+            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
             URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
                 data: try! JSONEncoder().encode(keyModel),
                 response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
@@ -107,7 +107,7 @@ final class TIMStorageInternalTests: XCTestCase {
             let storage = dataStorage(for: encryptionMethod)
 
             // Store refresh token with new password
-            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, password: "1234")
+            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
             URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
                 data: try! JSONEncoder().encode(keyModel),
                 response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
@@ -149,6 +149,179 @@ final class TIMStorageInternalTests: XCTestCase {
         }
     }
 
+    func testEnableBiometricAccessForRefreshTokenViaLongSecret() {
+        for encryptionMethod in TIMESEncryptionMethod.allCases {
+            let storage = dataStorage(for: encryptionMethod)
+
+            // Store refresh token with new password
+            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
+            URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
+                data: try! JSONEncoder().encode(keyModel),
+                response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            )
+
+            // Enable biometric access
+            XCTAssertFalse(storage.hasBiometricAccessForRefreshToken(userId: testRefreshToken.userId!))
+            let result = storage.enableBiometricAccessForRefreshToken(longSecret: keyModel.longSecret!, userId: testRefreshToken.userId!)
+            switch result {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            case .success:
+                XCTAssertTrue(storage.hasBiometricAccessForRefreshToken(userId: testRefreshToken.userId!))
+            }
+        }
+    }
+
+    func testStoreRefreshTokenWithLongSecret() {
+        let newRefreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjk1MTYyMzkwMjJ9.fzHyQ0D6kSOr-6i4gEiJoOm5UutfqgivtqtXbwaRv1c"
+
+        for encryptionMethod in TIMESEncryptionMethod.allCases {
+            let storage = dataStorage(for: encryptionMethod)
+
+            // Store refresh token with new password
+            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
+            URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
+                data: try! JSONEncoder().encode(keyModel),
+                response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            )
+
+            let expect = XCTestExpectation(description: "Storage failed to return")
+            let jwt = JWT(token: newRefreshToken)!
+            storage.storeRefreshTokenWithLongSecret(jwt, longSecret: "longSecret") { (result) in
+                switch result {
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                case .success:
+                    XCTAssertTrue(storage.hasRefreshToken(userId: newRefreshToken.userId!))
+                }
+                expect.fulfill()
+            }
+            wait(for: [expect], timeout: 1.0)
+        }
+    }
+
+    func testClear() {
+        for encryptionMethod in TIMESEncryptionMethod.allCases {
+            let storage = dataStorage(for: encryptionMethod)
+
+            // Store refresh token with new password
+            let keyModel = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: self.testRefreshToken, password: "1234")
+            URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
+                data: try! JSONEncoder().encode(keyModel),
+                response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            )
+            XCTAssertTrue(storage.hasRefreshToken(userId: testRefreshToken.userId!))
+
+            let expect1 = XCTestExpectation(description: "Storage should have returned.")
+            storage.enableBiometricAccessForRefreshToken(password: "1234", userId: testRefreshToken.userId!, completion: { result in
+                switch result {
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                case .success: break
+                }
+                expect1.fulfill()
+            })
+            wait(for: [expect1], timeout: 1.0)
+            XCTAssertTrue(storage.hasBiometricAccessForRefreshToken(userId: testRefreshToken.userId!))
+
+            storage.clear(userId: testRefreshToken.userId!)
+            XCTAssertEqual(0, storage.availableUserIds.count)
+            XCTAssertFalse(storage.hasRefreshToken(userId: testRefreshToken.userId!))
+            XCTAssertFalse(storage.hasBiometricAccessForRefreshToken(userId: testRefreshToken.userId!))
+        }
+    }
+
+    func testMultipleUsers() {
+        let user1RefreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjk1MTYyMzkwMjJ9.El5bSmm8IPR4M11wg6mMCwnlx2hP7x4XZiaORoTWafY"
+        let user2RefreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjk1MTYyMzkwMjJ9.q0FBllJKYNGIDEsHj8d0yIGLCaANkyjxER_l1Xm4P50"
+
+        XCTAssertNotEqual(user1RefreshToken, user2RefreshToken)
+        XCTAssertNotEqual(user1RefreshToken.userId!, user2RefreshToken.userId!)
+
+        for encryptionMethod in TIMESEncryptionMethod.allCases {
+            let storage = dataStorage(for: encryptionMethod)
+
+            // Store refresh tokens with new passwords
+            let keyModel1 = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: user1RefreshToken, password: "1234")
+            let keyModel2 = storeRefreshTokenWithNewPassword(dataStorage: storage, refreshToken: user2RefreshToken, password: "4321")
+            XCTAssertNotEqual(keyModel1.keyId, keyModel2.keyId)
+            XCTAssertTrue(storage.hasRefreshToken(userId: user1RefreshToken.userId!))
+            XCTAssertTrue(storage.hasRefreshToken(userId: user2RefreshToken.userId!))
+            XCTAssertEqual(2, storage.availableUserIds.count)
+
+            // Enable bio for user 1
+            URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
+                data: try! JSONEncoder().encode(keyModel1),
+                response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            )
+            let expect1 = XCTestExpectation(description: "Storage should have returned.")
+            storage.enableBiometricAccessForRefreshToken(password: "1234", userId: user1RefreshToken.userId!, completion: { result in
+                switch result {
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                case .success: break
+                }
+                expect1.fulfill()
+            })
+            wait(for: [expect1], timeout: 1.0)
+            XCTAssertTrue(storage.hasBiometricAccessForRefreshToken(userId: user1RefreshToken.userId!))
+            XCTAssertFalse(storage.hasBiometricAccessForRefreshToken(userId: user2RefreshToken.userId!))
+
+            // Get refresh token via bio for user 1
+            let expect2 = XCTestExpectation(description: "Bio result never returned for user 1")
+            storage.getStoredRefreshTokenViaBiometric(userId: user1RefreshToken.userId!, completion: { result in
+                switch result {
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                case .success(let bioResult):
+                    XCTAssertEqual(user1RefreshToken, bioResult.refreshToken.token)
+                }
+                expect2.fulfill()
+            })
+            wait(for: [expect2], timeout: 1.0)
+
+            // Get refresh token via bio for user 2 -> This should fail!
+            URLSessionStubResults.resultsForUrls[Self.getUrl] = .dataResponse(
+                data: try! JSONEncoder().encode(keyModel2),
+                response: HTTPURLResponse(url: Self.getUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            )
+            let expect3 = XCTestExpectation(description: "Bio result never returned for user 2")
+            storage.getStoredRefreshTokenViaBiometric(userId: user2RefreshToken.userId!, completion: { result in
+                switch result {
+                case .failure(let error):
+                    if case TIMError.storage(.encryptedStorageFailed(.secureStorageFailed(.failedToLoadData))) = error {
+                        // All good!
+                    } else {
+                        XCTFail("Should have caused failed to load data!")
+                    }
+                case .success:
+                    XCTFail("There should not be bio access for user2!")
+                }
+                expect3.fulfill()
+            })
+
+            // Get refresh token via password for user 2
+            let expect4 = XCTestExpectation(description: "Refresh token never returned for user 2")
+            storage.getStoredRefreshToken(userId: user2RefreshToken.userId!, password: "4321") { (result) in
+                switch result {
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                case .success(let token):
+                    XCTAssertEqual(user2RefreshToken, token.token)
+                }
+                expect4.fulfill()
+            }
+            wait(for: [expect3, expect4], timeout: 1.0)
+
+            // Delete user 2 and check that user 1 is still intact.
+            storage.clear(userId: user2RefreshToken.userId!)
+            XCTAssertEqual(1, storage.availableUserIds.count)
+            XCTAssertFalse(storage.hasRefreshToken(userId: user2RefreshToken.userId!))
+            XCTAssertTrue(storage.hasRefreshToken(userId: user1RefreshToken.userId!))
+            XCTAssertTrue(storage.hasBiometricAccessForRefreshToken(userId: user1RefreshToken.userId!))
+        }
+    }
+
 
     // MARK: - Private helpers
     private func dataStorage(for encryptionMethod: TIMESEncryptionMethod) -> TIMDataStorageInternal<SecureStorageMock> {
@@ -167,7 +340,7 @@ final class TIMStorageInternalTests: XCTestCase {
         )
     }
 
-    private func storeRefreshTokenWithNewPassword(dataStorage: TIMDataStorageInternal<SecureStorageMock>, password: String) -> TIMKeyModel {
+    private func storeRefreshTokenWithNewPassword(dataStorage: TIMDataStorageInternal<SecureStorageMock>, refreshToken: JWTString, password: String) -> TIMKeyModel {
         let createdKeyModel = TIMKeyModel(
             keyId: UUID().uuidString,
             key: "S2JQZVNoVm1ZcTN0Nnc5eQ==",
@@ -177,7 +350,7 @@ final class TIMStorageInternalTests: XCTestCase {
             data: try! JSONEncoder().encode(createdKeyModel),
             response: HTTPURLResponse(url: Self.createUrl, statusCode: 200, httpVersion: nil, headerFields: nil)!
         )
-        let refreshTokenJwt = JWT(token: testRefreshToken)!
+        let refreshTokenJwt = JWT(token: refreshToken)!
         let expect = XCTestExpectation(description: "Store refresh token with new password")
         XCTAssertFalse(dataStorage.availableUserIds.contains(refreshTokenJwt.userId))
         dataStorage.storeRefreshToken(refreshTokenJwt, withNewPassword: password) { (result) in
